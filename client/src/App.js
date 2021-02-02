e76541828b233a7149903af7825a678cff91c0b4
marenda <markostevic96@hotmail.com>
2021-02-02 17:22:32 +0100

    Display Profiles

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,13 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+
 
 
 if (localStorage.token) {
@@ -39,6 +41,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
 const Spinner = () => {
-    <Fragment>
+    return <Fragment>
         <img
             src={spinner}
             style={{ width: '200px', margin: 'auto', display: 'block' }}
